43c6adb71df194481c11ba1b5c21b01734d19303
Enesince98 <enes98lir@gmail.com>
2022-06-11 19:38:29 +0300

    Protected Routes Added
    Installed 'react-router-dom' and added route rules to both index.js and app.js files.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+import Missing from "./components/Missing";
+import Unauthorized from "./components/Unauthorized";
+import RequireAuth from "./components/RequireAuth";
+import { Routes, Route } from "react-router-dom";
+import ContentTypeBuilder from "./components/ContentTypeBuilder";
+import ContentTypeManager from "./components/ContentTypeManager";
+import Contents from "./components/Contents";
+import UserManager from "./components/UserManager";
+
+/*const ROLES = {
+  User: 2001,
+  Editor: 1984,
+  Admin: 5150,
+};*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* we want to protect these routes */}
+        {/*allowedRoles={[ROLES.User]}*/}
+        <Route element={<RequireAuth  />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        {/*allowedRoles={[ROLES.Editor, ROLES.Admin]}*/}
+        <Route
+          element={<RequireAuth />}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Route path="content-type-builder" element={<ContentTypeBuilder />} />
+        </Route>
+        {/*allowedRoles={[ROLES.Editor, ROLES.Admin]}*/}
+        <Route
+          element={<RequireAuth />}
+        >
+          <Route
+            path="content-type-manager/*"
+            element={<ContentTypeManager />}
+          />
+        </Route>
+        {/*allowedRoles={[ROLES.Editor, ROLES.Admin]}*/}
+        <Route
+          element={<RequireAuth />}
+        >
+          <Route path="contents" element={<Contents />} />
+        </Route>
+        {/*allowedRoles={[ROLES.Editor, ROLES.Admin]}*/}
+        <Route
+          element={<RequireAuth />}
+        >
+          <Route path="user-manager" element={<UserManager />} />
+        </Route>
+        {/*allowedRoles={[ROLES.Editor, ROLES.Admin]}*/}
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
