2b70b67de502108c2a17bbb1caae58c78306f1a4
Enesince98 <enes98lir@gmail.com>
2022-06-11 19:41:14 +0300

    Added AuthProvider
    AuthProvider use auth state from login page to provide authentication for all protected pages.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AuthProvider } from './context/AuthProvider';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthProvider>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
     </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
